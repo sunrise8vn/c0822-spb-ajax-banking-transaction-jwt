b9aa050be9b13a950cacda05b0151bd439b5dd02
sunrise8vn <sunrise8vn@gmail.com>
2023-01-30 17:06:30 +0700

    Init

---
+++ src/main/java/com/cg/model/Customer.java
@@ -0,0 +1,36 @@
+package com.cg.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "customers")
+@Accessors(chain = true)
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String phone;
+
+    @Column(precision = 12, scale = 0, nullable = false, updatable = false)
+    private BigDecimal balance;
+}
